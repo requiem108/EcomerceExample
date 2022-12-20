b01df5cd4578317bd0c46dd50ed578fd4f2cae28
isaias <mori8307@gmail.com>
2022-12-19 20:31:38 -0600

    nodemon and express

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
-import express from 'express';
-import data from './data.js';
+const express = require('express')
+const data = require('./data.js');
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/api/products', (req,res)=>{
     res.send(data.products)
 })
 
-const port = process.env.PORT || 5000;
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
-});
+})
