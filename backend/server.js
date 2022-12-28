5b093204116fc4a0afb903c8e58a5c4e8dab24c1
isaias <mori8307@gmail.com>
2022-12-28 11:17:45 -0600

    lesson 13 server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ app.get('/api/products', (req,res)=>{
     res.send(data.products)
 })
 
+app.get('/api/products/slug/:slug', (req,res)=>{
+    const product = data.products.find(x => x.slug === req.params.slug)
+    if(product){
+        res.send(product)
+    }else{
+        res.send(404).send({message:'Producto no encontrado'})
+    }
+})
+
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
 })
