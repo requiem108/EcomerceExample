2fcfdb19385952fa2827f59127627557394159f4
isaias <mori8307@gmail.com>
2022-12-28 10:34:27 -0600

    leson 11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,35 @@
 import {BrowserRouter,Routes, Route,Link} from 'react-router-dom'
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import {LinkContainer} from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header >
           <Navbar bg="dark" variant="dark">
             <Container>
-              
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
             </Container>
           </Navbar>
-          <Link to="/">amazona</Link>
+         
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
-            
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>            
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom"
 //import data from "../data"
 import axios from "axios";
 import logger from 'use-reducer-logger'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Product from "../components/Product";
 
 const reducer = (state, action) =>{
 
@@ -56,25 +59,17 @@ function HomeScreen(){
               loading? (<div>Loading...</div>)
               :error? (<div>{error}</div>):
                (
-              products.map(product => (
-              <div className="product" key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>              
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p><strong>${product.price}</strong></p>
-                  <button>Add to cart</button>
-                </div>    
-
-              </div>
-              ))
+              <Row>{  
+                products.map(product => (
+                  <Col key={product.slug} sm={6} md={5} lg={4} className="mb-3">
+                    <Product product={product}></Product>
+                  </Col>
+                ))
+              }</Row>
               )
             }
+            
           </div>      
     </div>
 }
-
 export default HomeScreen
